16c9b5f5daeda2b1ea20c670f9d8712f5248dd6f
Pascal Maugeri <pmaugeri2@gmail.com>
2018-06-11 23:51:28 +0200

    Initial version (beta).

---
+++ cli.py
@@ -0,0 +1,175 @@
+import sys
+import requests
+import json
+from akamai.edgegrid import EdgeGridAuth, EdgeRc
+from urlparse import urljoin
+
+
+
+from cmd import Cmd
+
+# Global variables
+sess = None
+baseurl = None
+currentAccount = 'current-account'
+promptText = '> '
+
+CONST_EVENT_TEMPLATE = 'event-template.json'
+
+
+
+
+class MyPrompt(Cmd):
+
+    def emptyline(self):
+         pass
+
+    def updatePrompt(self):
+        
+        global currentAccount
+
+        if (currentAccount != None):
+            promptText = currentAccount + '> '
+        else:
+            promptText = '> '
+
+        self.prompt = promptText
+
+
+    def do_cd(self, args):
+
+        global baseurl
+        global sess
+        global currentAccount
+
+        if (args == '..'):
+            currentAccount = 'current-account'
+            self.updatePrompt()   
+        else:
+            if (args == ''):
+                self.updatePrompt()
+            else: 
+                currentAccount = args
+                self.updatePrompt()
+
+
+    def do_get(self, args):
+        """Retrieve an Event and displays its details.\nGet Event summary: get <Event ID>\nGet Event services: get <Event ID> services"""
+        global baseurl
+        global sess
+
+        if (args != None):
+            params = args.split(' ')
+
+            # Show Event Summary
+            if (len(params) == 1):
+                url_path = '/events/v2/' + currentAccount + '/events/' + params[0]
+                result = sess.get(urljoin(baseurl, url_path))
+                if (result.status_code == 200):
+                    print json.dumps(result.json(), sort_keys=True, indent=2, separators=(',', ': '))
+                else:
+                    print 'ERROR: ' + str(result.status_code) + ' ' + result.reason
+
+            # Show Event Details
+            if (len(params) == 2 and params[1] == "services"):
+                url_path = '/events/v2/' + currentAccount + '/events/' + params[0] + '?services=detail'
+                result = sess.get(urljoin(baseurl, url_path))
+                if (result.status_code == 200):
+                    print json.dumps(result.json(), sort_keys=True, indent=2, separators=(',', ': '))
+                else:
+                    print 'ERROR: ' + str(result.status_code) + ' ' + result.reason
+
+
+    def do_create(self, args):
+        """\nCreate a new Event based on a JSON file called 'event-template.json'.\n\ncreate <Event Name> <Start Time (in milliseconds)> <End Time (in milliseconds)>\n"""
+        global baseurl
+        global sess
+
+        if (args != None):
+            params = args.split(' ')
+
+            # 
+            if (len(params) == 3):
+
+                eventName = params[0]
+                startTime = params[1]
+                endTime = params[2]
+
+                with open(CONST_EVENT_TEMPLATE, "r") as read_file:
+                    data = json.load(read_file)
+
+                data["name"] = eventName
+                data["start"] = startTime
+                data["end"] = endTime
+
+                url_path = '/events/v2/' + currentAccount + '/events/'
+                result = sess.post(urljoin(baseurl, url_path), json.dumps(data), headers={'Content-Type': 'application/json'})
+                if (result.status_code == 200):
+                    print json.dumps(result.json(), sort_keys=True, indent=2, separators=(',', ': '))
+                else:
+                    print 'ERROR: ' + str(result.status_code) + ' ' + result.reason
+
+    def do_ls(self, args):
+        """\nList all events in a given time range.\n\nls <Start Range (in milliseconds)> <End Range (in milliseconds)>\n"""
+        global baseurl
+        global sess
+
+        if (args != None):
+            params = args.split(' ')
+            if (len(params) == 2):
+
+                startRange = params[0]
+                endRange = params[1]
+                url_path = '/events/v2/' + currentAccount + '/events?startRange=' + startRange + '&endRange=' + endRange
+                result = sess.get(urljoin(baseurl, url_path))
+                if (result.status_code == 200):
+                    print json.dumps(result.json(), sort_keys=True, indent=2, separators=(',', ': '))
+                else:
+                    print 'ERROR: ' + str(result.status_code) + ' ' + result.reason
+
+
+
+    def do_rm(self, args):
+        """\nDelete an Event: rm <Event ID>\n"""
+        global baseurl
+        global sess
+
+        if (args != None):
+            params = args.split(' ')
+            if (len(params) == 1):
+                eventId = params[0]
+                print "Removing Event ID " + eventId + "..."
+                url_path = '/events/v2/' + current-account + '/events/' + eventId
+                result = sess.delete(urljoin(baseurl, url_path))
+                if (result.status_code == 200):
+                    print "Event ID " + eventId + " successfully removed."
+                else:
+                    print 'ERROR: ' + str(result.status_code) + ' ' + result.reason
+
+
+
+    def do_quit(self, args):
+        """Quits the program."""
+        print "Quitting."
+        raise SystemExit
+
+    def do_q(self, args):
+        """Quits the program."""
+        self.do_quit(args);
+
+
+
+if __name__ == '__main__':
+
+    # Initialize credentials for Akamai {OPEN} API
+    edgerc = EdgeRc('.edgerc')
+    section = sys.argv[1]
+    baseurl = 'https://%s' % edgerc.get(section, 'host')
+    sess = requests.Session()
+    sess.auth = EdgeGridAuth.from_edgerc(edgerc, section)
+
+    prompt = MyPrompt()
+    prompt.prompt = promptText
+    prompt.updatePrompt()
+    prompt.intro = 'Welcome to CLI for Akamai Event Center'
+    prompt.cmdloop('Starting prompt...')
